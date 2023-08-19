15acf42d3b9ec170946b840c4cf2b362b3524512
Sunny <sunmeiappprep@gmail.com>
2023-08-19 01:10:01 -0400

    add delay to filter, Button works

--- popup.js
+++ popup.js
@@ -1,23 +1,29 @@
 // adding a new bookmark row to the popup
 
-const viewBookmarks = () => { };
+function setupSubmitButton() {
+    const inputElement = document.getElementById("myInput");
+    const submitButton = document.getElementById("submitButton");
+  
+    submitButton.addEventListener("click", () => {
+      const inputValue = inputElement.value;
+  
+      // Send a message to contentScript.js with the input value
+      chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
+        const activeTab = tabs[0];
+        chrome.tabs.sendMessage(activeTab.id, { action: "addKeyToFilterWords", value: inputValue }, response => {
+          // Handle response if needed
+          console.log(response);
+        });
+      });
+    });
+  }
+  // Call the function to set up the submit button
+  setupSubmitButton();
+  
 
-const onPlay = e => { };
-
-const onDelete = e => { };
-
-const setBookmarkAttributes = () => { };
-
-// const addNewBookmark = () => {
-//     // Your function logic here
-//     console.log("Add Bookmark function called");
-// };
-
-// // Get a reference to the button element
-// const addButton = document.getElementById("addBookmarkButton2");
-
-// // Attach a click event listener to the button
-// addButton.addEventListener("click", addNewBookmark);
-
-
-document.addEventListener("DOMContentLoaded", () => { });
+// document.addEventListener("DOMContentLoaded", () => {
+//     const submitButton = document.getElementById("submitButton");
+//     submitButton.addEventListener("click", handleSubmitButtonClick);
+//     const invokeButton = document.getElementById("invokeButton");
+//     invokeButton.addEventListener("click", sendMessageToContentScript);
+// });
